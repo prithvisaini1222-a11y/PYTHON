b093055893ef1ba809983c7618721ddbe963341d
prithvisaini1222-a11y <prithvisaini1222@gmail.com>
2026-08-05 22:43:02 +0530

    Add student profile display in studentprofile.py

---
+++ studentprofile.py
@@ -0,0 +1,12 @@
+print("==========Student Profile==========")
+Name="Prithvi"
+Age=19
+College="KRMU"
+Branch="CSE (AI&ML)"
+Cgpa=9.14
+print("Name:",Name)
+print("Age:",Age)
+print("College:",College)
+print("Branch:",Branch)
+print("CGPA:",Cgpa)
+print("====================================")
